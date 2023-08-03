b12153ff6481c97e9b3257bf68fb7fa3a589aadd
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-08-03 13:40:19 -0300

    criando a validação nos campos de altura e peso

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,98 @@
 import { useState } from "react";
-import { Container } from "./styles";
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Modal } from "../../components/Modal";
-import { FiArrowRight } from "react-icons/fi";
+import { Error } from "../../components/Error";
+
+import { Container } from "./styles";
 
 export const Home = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [errorPopup, setErrorPopup] = useState(false);
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [finalResult, setFinalResult] = useState("");
 
-  const handleOpenModal = () => {
-    setOpenModal(true);
+  // Função para abrir o Modal
+  const handleOpenModal = () => setOpenModal(true);
+
+  // Função para fechar o Modal
+  const handleCloseModal = () => setOpenModal(false);
+
+  // Função para classificar o IMC de acordo com o resultado
+  const getImcRating = (imc) => {
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 24.9) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc < 29.9) {
+      return "Acima do peso";
+    } else {
+      return "Obeso";
+    }
   };
 
-  const handleCloseModal = () => {
-    setOpenModal(false);
+  // Função para calcular o IMC e exibir o Modal
+  const calculateImc = () => {
+    const convertedWeight = parseFloat(weight);
+    const convertedHeight = parseFloat(height);
+
+    // Verificação para saber se os números são válidos, se não, renderiza o componente de <Erro />
+    if (isNaN(convertedWeight) || isNaN(convertedHeight)) {
+      setErrorPopup(true);
+      return;
+    }
+
+    // Calcula o IMC e guarda na const imc
+    const imc = (convertedWeight / (convertedHeight / 100) ** 2).toFixed(2);
+
+    // Guarda a classificação do IMC na const rating
+    const rating = getImcRating(imc);
+
+    // Define o resultado no estado, abre o modal e reseta os campos de peso e altura
+    setFinalResult(`Seu IMC é ${imc} e sua classificação é: ${rating}`);
+    handleOpenModal();
+    setHeight("");
+    setWeight("");
   };
 
   return (
     <Container>
       <form>
         <h1>Calcule seu IMC</h1>
+
         <div>
           <label htmlFor="weight">Peso (kg)</label>
-          <Input id="weight" placeholder="60" />
+          <Input
+            id="weight"
+            value={weight}
+            placeholder="60"
+            onChange={(e) => {
+              setWeight(e.target.value);
+              setErrorPopup(false);
+            }}
+          />
         </div>
+
         <div>
           <label htmlFor="height">Altura (cm)</label>
-          <Input id="height" placeholder="165" />
+          <Input
+            id="height"
+            value={height}
+            placeholder="165"
+            onChange={(e) => {
+              setHeight(e.target.value);
+              setErrorPopup(false);
+            }}
+          />
         </div>
-        <Button
-          title="Calcular IMC"
-          icon={FiArrowRight}
-          onClick={handleOpenModal}
-        ></Button>
+
+        <Button title="Calcular IMC" onClick={calculateImc} />
       </form>
-      {openModal && (
-        <Modal result="Seu IMC é: 22.65" onClose={handleCloseModal} />
-      )}
+
+      {errorPopup && <Error />}
+      {openModal && (<Modal message={finalResult} onClose={handleCloseModal}></Modal>)}
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,26 +6,35 @@ export const Container = styled.div`
   place-content: center;
 
   form {
-    max-width: 428px;
-    width: 100%;
-    margin: 0 auto;
+    width: 360px;
     background-color: ${({ theme }) => theme.colors.white};
     box-shadow: rgba(0, 0, 0, 0.15) 2px 2px 3px;
     border-radius: 6px;
-    padding: 32px 40px;
+    padding: 44px;
 
     > h1 {
       color: ${({ theme }) => theme.colors.blue_2};
-      font-size: 32px;
+      font-size: 28px;
       font-weight: bold;
-      line-height: 64px;
+      margin-bottom: 26px;
+
+      @media (max-width: 320px) {
+        font-size: 24px;
+      }
     }
 
     label {
       display: block;
       font-size: 14px;
-      margin-bottom: 8px;
       color: ${({ theme }) => theme.colors.green};
+    }
+
+    @media (max-width: 380px) {
+      width: 320px;
+    }
+
+    @media (max-width: 320px) {
+      width: 290px;
     }
   }
 `;
